cb49a2957ad578622e0b740aaa65cb9684e0de59
maiyao1988 <>
2017-10-05 00:15:04 +0800

    消warning，整理

--- fix.c
+++ fix.c
@@ -4,16 +4,21 @@
 static const char* g_str = "..dynsym..dynstr..hash..rel.dyn..rel.plt..plt..text@.ARM.extab..ARM.exidx..fini_array..init_array..dynamic..got..data..bss..shstrtab\0";
 static const char* g_strtabcontent = "\0.dynsym\0.dynstr\0.hash\0.rel.dyn\0.rel.plt\0.plt\0.text@.ARM.extab\0.ARM.exidx\0.fini_array\0.init_array\0.dynamic\0.got\0.data\0.bss\0.shstrtab\0";
 
+static Elf32_Word _get_off_in_shstrtab(const char *name)
+{
+	return (Elf32_Word)(strstr(g_str,".bss") - g_str);
+}
+
 //段表项
 static Elf32_Shdr g_shdr[SHDRS] = { 0 };
 
-void get_elf_header(Elf32_Ehdr *pehdr, const char *buffer)
+static void get_elf_header(Elf32_Ehdr *pehdr, const char *buffer)
 {
 	int header_len = sizeof(Elf32_Ehdr);
 	memcpy(pehdr, (void*)buffer, header_len);
 }
 
-long get_file_len(FILE* p)
+static long get_file_len(FILE* p)
 {
 	fseek (p, 0, SEEK_END);
 	long fsize = ftell (p);
@@ -22,7 +27,7 @@ long get_file_len(FILE* p)
 }
 
 
-void regen_section_header(const Elf32_Ehdr *pehdr, const char *buffer)
+static void regen_section_header(const Elf32_Ehdr *pehdr, const char *buffer)
 {
 	Elf32_Dyn* dyn = NULL;
 	Elf32_Phdr load = { 0 };
@@ -40,7 +45,7 @@ void regen_section_header(const Elf32_Ehdr *pehdr, const char *buffer)
 			//实际上取的是最后一个PT_LOAD
 			if (phdr[i].p_vaddr > 0x0) {
 				load = phdr[i];
-				g_shdr[BSS].sh_name = strstr(g_str,".bss") - g_str;
+				g_shdr[BSS].sh_name = _get_off_in_shstrtab(".bss");
 				g_shdr[BSS].sh_type = SHT_NOBITS;
 				g_shdr[BSS].sh_flags = SHF_WRITE | SHF_ALLOC;
 				g_shdr[BSS].sh_addr =  phdr[i].p_vaddr + phdr[i].p_filesz;
@@ -50,7 +55,7 @@ void regen_section_header(const Elf32_Ehdr *pehdr, const char *buffer)
 		}
 		else if(p_type == PT_DYNAMIC) {
 			//动态表，动态表包括很多项，找到动态表位置可以恢复大部分结构
-			g_shdr[DYNAMIC].sh_name = strstr(g_str, ".dynamic") - g_str;
+			g_shdr[DYNAMIC].sh_name = _get_off_in_shstrtab(".dynamic");
 			g_shdr[DYNAMIC].sh_type = SHT_DYNAMIC;
 			g_shdr[DYNAMIC].sh_flags = SHF_WRITE | SHF_ALLOC;
 			g_shdr[DYNAMIC].sh_addr = phdr[i].p_vaddr;
@@ -65,7 +70,7 @@ void regen_section_header(const Elf32_Ehdr *pehdr, const char *buffer)
 		}
 		
 		else if(phdr[i].p_type == PT_LOPROC || phdr[i].p_type == PT_LOPROC + 1) {
-			g_shdr[ARMEXIDX].sh_name = strstr(g_str, ".ARM.exidx") - g_str;
+			g_shdr[ARMEXIDX].sh_name = _get_off_in_shstrtab(".ARM.exidx");
 			g_shdr[ARMEXIDX].sh_type = SHT_LOPROC;
 			g_shdr[ARMEXIDX].sh_flags = SHF_ALLOC;
 			g_shdr[ARMEXIDX].sh_addr = phdr[i].p_vaddr;
@@ -88,7 +93,7 @@ void regen_section_header(const Elf32_Ehdr *pehdr, const char *buffer)
 		int tag = dyn[i].d_tag;
 		switch (tag) {
 			case DT_SYMTAB:
-				g_shdr[DYNSYM].sh_name = strstr(g_str, ".dynsym") - g_str;
+				g_shdr[DYNSYM].sh_name = _get_off_in_shstrtab(".dynsym");
 				g_shdr[DYNSYM].sh_type = SHT_DYNSYM;
 				g_shdr[DYNSYM].sh_flags = SHF_ALLOC;
 				g_shdr[DYNSYM].sh_addr = dyn[i].d_un.d_ptr;
@@ -100,7 +105,7 @@ void regen_section_header(const Elf32_Ehdr *pehdr, const char *buffer)
 				break;
 				
 			case DT_STRTAB:
-				g_shdr[DYNSTR].sh_name = strstr(g_str, ".dynstr") - g_str;
+				g_shdr[DYNSTR].sh_name = _get_off_in_shstrtab(".dynstr");
 				g_shdr[DYNSTR].sh_type = SHT_STRTAB;
 				g_shdr[DYNSTR].sh_flags = SHF_ALLOC;
 				g_shdr[DYNSTR].sh_offset = dyn[i].d_un.d_ptr;
@@ -110,7 +115,7 @@ void regen_section_header(const Elf32_Ehdr *pehdr, const char *buffer)
 				break;
 				
 			case DT_HASH:
-				g_shdr[HASH].sh_name = strstr(g_str, ".hash") - g_str;
+				g_shdr[HASH].sh_name = _get_off_in_shstrtab(".hash");
 				g_shdr[HASH].sh_type = SHT_HASH;
 				g_shdr[HASH].sh_flags = SHF_ALLOC;
 				g_shdr[HASH].sh_addr = dyn[i].d_un.d_ptr;
@@ -125,7 +130,7 @@ void regen_section_header(const Elf32_Ehdr *pehdr, const char *buffer)
 				break;
 				
 			case DT_REL:
-				g_shdr[RELDYN].sh_name = strstr(g_str, ".rel.dyn") - g_str;
+				g_shdr[RELDYN].sh_name = _get_off_in_shstrtab(".rel.dyn");
 				g_shdr[RELDYN].sh_type = SHT_REL;
 				g_shdr[RELDYN].sh_flags = SHF_ALLOC;
 				g_shdr[RELDYN].sh_addr = dyn[i].d_un.d_ptr;
@@ -137,7 +142,7 @@ void regen_section_header(const Elf32_Ehdr *pehdr, const char *buffer)
 				break;
 				
 			case DT_JMPREL:
-				g_shdr[RELPLT].sh_name = strstr(g_str, ".rel.plt") - g_str;
+				g_shdr[RELPLT].sh_name = _get_off_in_shstrtab(".rel.plt");
 				g_shdr[RELPLT].sh_type = SHT_REL;
 				g_shdr[RELPLT].sh_flags = SHF_ALLOC;
 				g_shdr[RELPLT].sh_addr = dyn[i].d_un.d_ptr;
@@ -153,7 +158,7 @@ void regen_section_header(const Elf32_Ehdr *pehdr, const char *buffer)
 				break;
 				
 			case DT_FINI:
-				g_shdr[FINIARRAY].sh_name = strstr(g_str, ".fini_array") - g_str;
+				g_shdr[FINIARRAY].sh_name = _get_off_in_shstrtab(".fini_array");
 				g_shdr[FINIARRAY].sh_type = 15;
 				g_shdr[FINIARRAY].sh_flags = SHF_WRITE | SHF_ALLOC;
 				g_shdr[FINIARRAY].sh_offset = dyn[i].d_un.d_ptr;
@@ -163,7 +168,7 @@ void regen_section_header(const Elf32_Ehdr *pehdr, const char *buffer)
 				break;
 				
 			case DT_INIT:
-				g_shdr[INITARRAY].sh_name = strstr(g_str, ".init_array") - g_str;
+				g_shdr[INITARRAY].sh_name = _get_off_in_shstrtab(".init_array");
 				g_shdr[INITARRAY].sh_type = 14;
 				g_shdr[INITARRAY].sh_flags = SHF_WRITE | SHF_ALLOC;
 				g_shdr[INITARRAY].sh_offset = dyn[i].d_un.d_ptr;
@@ -181,7 +186,7 @@ void regen_section_header(const Elf32_Ehdr *pehdr, const char *buffer)
 				break;
 				
 			case DT_PLTGOT:
-				g_shdr[GOT].sh_name = strstr(g_str, ".got") - g_str;
+				g_shdr[GOT].sh_name = _get_off_in_shstrtab(".got");
 				g_shdr[GOT].sh_type = SHT_PROGBITS;
 				g_shdr[GOT].sh_flags = SHF_WRITE | SHF_ALLOC;
 				//TODO:这里基于假设.got一定在.dynamic段之后，并不可靠，王者荣耀libGameCore.so就是例外
@@ -204,7 +209,7 @@ void regen_section_header(const Elf32_Ehdr *pehdr, const char *buffer)
 			g_shdr[GOT].sh_size = gotEnd - gotBase;
 			//假定DATA紧接着GOT
 			//此时GOT才是可靠的值，才能用来修复
-			g_shdr[DATA].sh_name = strstr(g_str, ".data") - g_str;
+			g_shdr[DATA].sh_name = _get_off_in_shstrtab(".data");
 			g_shdr[DATA].sh_type = SHT_PROGBITS;
 			g_shdr[DATA].sh_flags = SHF_WRITE | SHF_ALLOC;
 			g_shdr[DATA].sh_addr = g_shdr[GOT].sh_addr + g_shdr[GOT].sh_size;
@@ -217,7 +222,7 @@ void regen_section_header(const Elf32_Ehdr *pehdr, const char *buffer)
 		{
 			//.got紧接着.dynamic的假设不成立
 			//无法修复GOT，全部清零，以免影响ida分析
-			printf("warning GOT is not after dynamic\n");
+			printf("warning .got is not after .dynamic can't fix .got\n");
 			memset(&g_shdr[GOT], 0, sizeof(Elf32_Shdr));
 		}
 	}
@@ -228,7 +233,7 @@ void regen_section_header(const Elf32_Ehdr *pehdr, const char *buffer)
 	g_shdr[FINIARRAY].sh_size = g_shdr[INITARRAY].sh_addr - g_shdr[FINIARRAY].sh_addr;
 	g_shdr[INITARRAY].sh_size = g_shdr[DYNAMIC].sh_addr - g_shdr[INITARRAY].sh_addr;
 	
-	g_shdr[PLT].sh_name = strstr(g_str, ".plt") - g_str;
+	g_shdr[PLT].sh_name = _get_off_in_shstrtab(".plt");
 	g_shdr[PLT].sh_type = SHT_PROGBITS;
 	g_shdr[PLT].sh_flags = SHF_ALLOC | SHF_EXECINSTR;
 	g_shdr[PLT].sh_addr = g_shdr[RELPLT].sh_addr + g_shdr[RELPLT].sh_size;
@@ -236,18 +241,18 @@ void regen_section_header(const Elf32_Ehdr *pehdr, const char *buffer)
 	g_shdr[PLT].sh_size = (20 + 12 * (g_shdr[RELPLT].sh_size) / sizeof(Elf32_Rel));
 	g_shdr[PLT].sh_addralign = 4;
 	
-	g_shdr[TEXT].sh_name = strstr(g_str, ".text") - g_str;
+	g_shdr[TEXT].sh_name = _get_off_in_shstrtab(".text");
 	g_shdr[TEXT].sh_type = SHT_PROGBITS;
 	g_shdr[TEXT].sh_flags = SHF_ALLOC | SHF_EXECINSTR;
 	g_shdr[TEXT].sh_addr = g_shdr[PLT].sh_addr + g_shdr[PLT].sh_size;
 	g_shdr[TEXT].sh_offset = g_shdr[TEXT].sh_addr;
 	g_shdr[TEXT].sh_size = g_shdr[ARMEXIDX].sh_addr - g_shdr[TEXT].sh_addr;
 	
-	g_shdr[STRTAB].sh_name = strstr(g_str, ".shstrtab") - g_str;
+	g_shdr[STRTAB].sh_name = _get_off_in_shstrtab(".shstrtab");
 	g_shdr[STRTAB].sh_type = SHT_STRTAB;
 	g_shdr[STRTAB].sh_flags = SHT_NULL;
 	g_shdr[STRTAB].sh_addr = 0;	//写文件的时候修正
-	g_shdr[STRTAB].sh_size = strlen(g_str) + 1;
+	g_shdr[STRTAB].sh_size = (Elf32_Word)strlen(g_str) + 1;
 	g_shdr[STRTAB].sh_addralign = 1;
 }
 
@@ -258,11 +263,16 @@ int main(int argc, char const *argv[])
 	Elf32_Ehdr ehdr = {0};
 	
 	if (argc < 2) {
-		printf("<so_path>\n");
+		printf("<src_so_path> [<out_so_path>]\n");
 		return -1;
 	}
-	
-	fr = fopen(argv[1],"rb");
+	const char *outPutPath = "fix.so";
+	if (argc > 2)
+	{
+		outPutPath = argv[2];
+	}
+	const char *openPath = argv[1];
+	fr = fopen(openPath,"rb");
 	//unsigned base = (unsigned)strtol(argv[2], 0, 16);
 	
 	if(fr == NULL) {
@@ -270,7 +280,7 @@ int main(int argc, char const *argv[])
 		goto error;
 	}
 	
-	int flen = get_file_len(fr);
+	size_t flen = get_file_len(fr);
 	
 	buffer = (char*)malloc(flen);
 	if (buffer == NULL) {
@@ -278,13 +288,13 @@ int main(int argc, char const *argv[])
 		goto error;
 	}
 	
-	unsigned long result = fread (buffer,1,flen,fr);
+	unsigned long result = fread (buffer, 1, flen, fr);
 	if (result != flen) {
-		printf("Reading error\n");
+		printf("Reading %s error\n", openPath);
 		goto error;
 	}
 	
-	fw = fopen("fix.so","wb");
+	fw = fopen(outPutPath,"wb");
 	if(fw == NULL) {
 		printf("Open failed: fix.so\n");
 		goto error;
@@ -292,7 +302,6 @@ int main(int argc, char const *argv[])
 	
 	get_elf_header(&ehdr, buffer);
 	//ehdr.e_entry = base;
-	
 	
 	regen_section_header(&ehdr, buffer);
 	
@@ -302,7 +311,7 @@ int main(int argc, char const *argv[])
 	ehdr.e_shstrndx = SHDRS - 1;
 	
 	//段表头紧接住段表最后一个成员--字符串段之后
-	ehdr.e_shoff = flen + len_gstr + 1;
+	ehdr.e_shoff = (Elf32_Off)(flen + len_gstr + 1);
 	
  	size_t szEhdr = sizeof(Elf32_Ehdr);
 	//Elf头
@@ -310,12 +319,12 @@ int main(int argc, char const *argv[])
 	//除了Elf头之外的原文件内容
 	fwrite(buffer+szEhdr, flen-szEhdr, 1, fw);
 	//补上段名字符串段
-	g_shdr[STRTAB].sh_offset = flen;
+	g_shdr[STRTAB].sh_offset = (Elf32_Off)flen;
 	
 	fwrite(g_strtabcontent, len_gstr + 1, 1, fw);
 	//补上段表头
 	fwrite(&g_shdr, sizeof(g_shdr), 1, fw);
-	
+	printf("fixed so has write to %s", outPutPath);
 error:
 	if(fw != NULL)
 		fclose(fw);
